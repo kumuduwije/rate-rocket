32f2a6a2543c535c554f9b1840ddf50640d2954a
Kumudu Wijewardhana <54832037+kumuduwije@users.noreply.github.com>
2024-02-01 12:32:35 +0530

    Prevent calculate negative values

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -371,7 +371,7 @@ export default function MainPage() {
                                 name={amountInSourceCurrency}
                                 className="block mb-2 text-sm font-medium text-gray-600 dark:text-white">Amount in Source Currency:</label>
 
-                            <input onChange={(e)=>setAmountInSourceCurrency(e.target.value)} type="number" id={amountInSourceCurrency}
+                            <input onChange={(e)=>setAmountInSourceCurrency(e.target.value)} type="number" min={1} id={amountInSourceCurrency}
                             name={amountInSourceCurrency}
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Example: 250" required />
                         </div>
